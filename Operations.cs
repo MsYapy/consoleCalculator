9158e74d3ec0f15166b84c5a15973cd9683a5ceb
Yesi Alexander Poveda Yara <yesi.poveda@applusglobal.com>
2024-05-07 16:15:18 -0500

    Basiccalculator

---
+++ Operations.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+
+namespace basicA
+{
+    internal class Operations
+    {
+
+        public decimal result { set; get; }
+        private decimal num1 = 0;
+        private decimal num2 = 0;
+
+        private decimal getValues()
+        {
+                Console.WriteLine("Ingrese el valor");
+                string values = Console.ReadLine();
+                decimal num = decimal.Parse(values);
+                return num;
+        }
+
+        public decimal DeleteResult() 
+        { 
+            result = 0;
+
+            return result;
+        }
+        public decimal sumar() 
+        {
+            if (result == 0) 
+            {
+                num1 = getValues();
+                num2 = getValues();
+                result = num1 + num2;   
+                return result;
+            }    
+            num2 = getValues(); 
+            result = num2 + result;
+            return result;   
+        }
+        public decimal Restar()
+        {
+            if (result == 0)
+            {
+                num1 = getValues();
+                num2 = getValues();
+                result = num1 - num2;
+                return result;
+            }
+            num2 = getValues();
+            result = result - num2;
+            return result;
+        }
+        public decimal Multiplicar()
+        {
+            if (result == 0)
+            {
+                num1 = getValues();
+                num2 = getValues();
+                result = num1 * num2;
+                return result;
+            }
+            num2 = getValues();
+            result = result * num2;
+            return result;
+        }
+        public decimal Divividir()
+        {
+            if (result == 0)
+            {
+                do
+                {
+                    Console.WriteLine("Escoja un numero diferente a 0");
+                    num1 = getValues();
+                }
+                while (num1 == 0);
+                num2 = getValues();
+                result = num1 / num2;
+                return result;
+            }
+            num2 = getValues();
+            result = result / num2;
+            return result;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
+using basicA;
+
+class Calculator
+{
+    public static void Main(string[] args)
+    {
+       
+        List<decimal> results = new List<decimal>();
+        int Op = 0;
+        do
+        {
+            if (results.Count != 0)
+            {
+               Console.WriteLine("v) Continuar operando sobre el resultado \nf) Iniciar nueva operacion");
+                string conti = Console.ReadLine();
+                if (conti == "v")
+                {
+                    Console.WriteLine($"Cantidad de resultados: {results.Count}");
+                    foreach (int i in results) 
+                    {
+                     Console.Write($"{i}, ");              
+                    }
+                    Console.WriteLine($"\nSeguir operando sobre el resultado {results[results.Count -1]}");
+                }
+                else{
+                  results.Clear();
+                  Console.WriteLine($"Continuar operando");  
+                }
+            }
+            Console.WriteLine(" Calculadora de consola \n"
+             + "1) Sumar \n2) Resta\n3) Multiplicar\n4) Dividir\n5) Salir");
+            string items = Console.ReadLine();
+            Op = int.Parse(items);
+
+            if (Op > 5 || Op < 1)
+            {
+                Console.WriteLine("*** Elija el numero segun las opciones del menu ***");
+            } 
+            else if (Op == 5) 
+            {
+                Console.WriteLine("BYE..");
+                Environment.Exit(0);
+            }
+            else
+            {
+
+                if (results.Count > 0) 
+                {
+                    results.Add(MenuCalculator(Op, results[results.Count - 1]));
+                } else
+                {
+                    results.Add(MenuCalculator(Op));
+                }
+              
+            }
+        }
+        while (Op != 5);
+    }
+
+
+    public static decimal MenuCalculator(int item, decimal r = 0)
+    {
+        Operations operations = new Operations();
+        if (r != 0) 
+        {
+            operations.result = r;  
+        }
+        switch (item)
+        {
+            case 1:
+                Console.WriteLine("Suma");
+                operations.sumar();
+                break;
+            case 2:
+                Console.WriteLine("Resta");
+                operations.Restar();
+                break;
+            case 3:
+                Console.WriteLine("Multiplicacion");
+                operations.Multiplicar();
+                break;
+            case 4:
+                Console.WriteLine("Dividir");
+                operations.Divividir(); 
+                break;
+            default:
+                Console.WriteLine();
+                break;
+        }
+        Console.WriteLine("Resultado = " +operations.result);
+        return operations.result;
+    }
+}
